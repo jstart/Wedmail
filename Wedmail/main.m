6ca7852e8121107a20ee6fa35355c2a6b70f160c
Christopher Truman <cleetruman@gmail.com>
2013-11-02 17:03:11 -0700

    Initial Commit

---
+++ Wedmail/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Wedmail
+//
+//  Created by Christopher Truman on 11/2/13.
+//  Copyright (c) 2013 truman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CLTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CLTAppDelegate class]));
+    }
+}
---
+++ WedmailTests/WedmailTests.m
@@ -0,0 +1,34 @@
+//
+//  WedmailTests.m
+//  WedmailTests
+//
+//  Created by Christopher Truman on 11/2/13.
+//  Copyright (c) 2013 truman. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WedmailTests : XCTestCase
+
+@end
+
+@implementation WedmailTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
